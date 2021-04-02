fd532c5105d32b6224424ccafd324a8997d08673
sunnysani <naufal.sani2001@gmail.com>
2021-04-03 01:50:01 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -6,6 +6,7 @@
 # useful, but WITHOUT ANY WARRANTY; without even the implied 
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+# REV05 Sat 27 Mar 00:12:28 WIB 2021
 # REV04 Mon 15 Mar 19:27:52 WIB 2021
 # REV03 Sun 14 Mar 18:21:27 WIB 2021
 # REV02 Fri 12 Mar 13:40:58 WIB 2021
@@ -17,12 +18,14 @@
 # Check it out with "gpg --list-key"
 
 REC2="naufal.sani2001@gmail.com"
-REC1="naufal.sani2001@gmail.com"
+REC1="operatingsystems@vlsm.org"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
 
 [ -d $HOME/RESULT ] || mkdir -p $HOME/RESULT
 pushd $HOME/RESULT
+echo ""
+
 for II in W?? ; do
     [ -d $II ] || continue
     TARFILE=my$II.tar.bz2
@@ -32,28 +35,35 @@ for II in W?? ; do
     tar cfj $TARFILE $II/
     echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
     gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+    echo""
 done
 popd
 
 rm -f $HOME/RESULT/fakeDODOL
-for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
+echo ""
+for II in $HOME/RESULT/myW*.tar.bz2.asc ; do
    echo "Check and move $II..."
    [ -f $II ] && mv -f $II .
 done
+echo ""
 
 echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
+echo ""
 
 echo "sha256sum $FILES > $SHA"
 sha256sum $FILES > $SHA
+echo ""
 
 echo "sha256sum -c $SHA"
 sha256sum -c $SHA
+echo ""
 
 echo "gpg -o $SHA.asc -a -sb $SHA"
 gpg -o $SHA.asc -a -sb $SHA
 
 echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
+echo ""
 
 exit 0
